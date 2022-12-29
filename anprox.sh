0eb2dc6bc4de058df9beafe9166b243b39ba333f
Cristian Gomez <97839792+Crissassun@users.noreply.github.com>
2022-12-29 14:39:04 -0600

    Update script
    Added the ssh copy password to the script

--- anprox.sh
+++ anprox.sh
@@ -48,13 +48,15 @@ check_exit_code "Ansible proxmox request installation"
 
 print_color "green" "Provide Proxmox IP"
 read -p "Enter the Proxmox IP address: " ip_address
+read -s -p "Enter the root proxmox password: " password
 
 print_color "green" "Generating SSH pair keys"
 ssh-keygen -t rsa
 check_exit_code "SSH keys"
 
 print_color "green" "Copy ssh pub key to $ip_address"
-ssh-copy-id root@$ip_address
+yum install sshpass -y
+sshpass -p $password ssh-copy-id root@$ip_address
 check_exit_code "SSH pub key copy"
 
 
